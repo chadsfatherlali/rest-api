aa832c3e963df3997ade1f7b72cfbf47e3aa445a
chadsfatherlali <chadsfather@gmail.com>
2022-03-16 12:18:14 -0500

    On branch master

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -14,6 +14,17 @@ const usersRoutes = (models) => {
     }
   })
 
+  users.get('/users/:id', async (req, res) => {
+    try {
+      const result = await models.Users
+        .findOne({ _id: req.params.id })
+
+      res.json(result)
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
   users.post('/users', async (req, res) => {
     try {
       const result = await models.Users
@@ -29,6 +40,17 @@ const usersRoutes = (models) => {
     try {
       const result = await models.Users
         .updateOne({ _id: req.params.id }, { $set: req.body })
+
+      res.json(result)
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
+  users.delete('/users/:id', async (req, res) => {
+    try {
+      const result = await models.Users
+        .deleteOne({ _id: req.params.id })
 
       res.json(result)
     } catch (error) {
